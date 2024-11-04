fb51e321e446c1287ba82239873381695297c604
liyongbiao <ligejiuye@gmail.com>
2024-11-04 11:19:57 +0800

    kvserver 的完整编写(一个强一致的

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -1,9 +1,7 @@
 package kvsrv
 
 import (
-	"fmt"
 	"log"
-	"strconv"
 	"sync"
 )
 
@@ -19,11 +17,9 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 type KVServer struct {
 	mu sync.Mutex
 
-	// 先来一个kvmap 存储所有数据
-	data   map[string]string
-	status map[int64]string // 记录不同的请求 id 的之前的result 结果
-	// status是一个缓存, 对于请求的缓存, 而data是一个持久化的数据, 如此设计的
-	// lastSeenRequest map[int64]bool // 记录重复请求, key是 request 的id , 如果是重复的请求, 就只能执行一个哦
+	// Your definitions here.
+	kvMap  map[string]string
+	status map[int64]string //key: arg id; value: previous result
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
@@ -32,90 +28,71 @@ func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 	if args.Confirm {
-		if _, ok := kv.status[args.RequestID]; ok {
-			delete(kv.status, args.RequestID)
+		if _, ok := kv.status[args.Id]; ok {
+			delete(kv.status, args.Id)
 		}
 		return
 	}
-	if _, ok := kv.status[args.RequestID]; ok {
-		reply.Value = kv.status[args.RequestID]
+	if _, ok := kv.status[args.Id]; ok {
+		reply.Value = kv.status[args.Id]
 	} else {
-		val := kv.data[key]
+		val := kv.kvMap[key]
 		reply.Value = val
-		kv.status[args.RequestID] = val
+		kv.status[args.Id] = val
 	}
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
+	// Your code here.
+	key := args.Key
 	kv.mu.Lock()
-	fmt.Println("request " + strconv.FormatInt(args.RequestID, 10) + "get lock")
-
-	defer kv.mu.Unlock() // get也需要 锁是因为 需要访问到 并发方便的一个map里边去
-
-	// 检查是否是重复请求
-	//	fmt.Println("request duplicated?")
-	fmt.Println(kv.status[args.RequestID]) // 测试到确实重复了, 已经出现了
-	if args.Confirm {
-		// 表示这是一个重复的确认请求, 此时server可以删除缓存啦
-		if _, ok := kv.status[args.RequestID]; ok {
-			delete(kv.status, args.RequestID)
-		}
-		return
-	}
-	if _, ok := kv.status[args.RequestID]; ok {
-		// 即没有confirm, 这个请求是第一次来
-		// 并且已经存在 旧值 缓存的情况 , 就直接返回旧值即可
-		// 这个相当于是一个缓存! 如果put缓存到了, 就直接返回
-		reply.Value = kv.status[args.RequestID]
-
-	} else {
-		// 不存在, 就需要更新 同时的两个map 了
-		val := kv.data[args.Key] // 将数据取给put 请求
-		reply.Value = val
-		kv.status[args.RequestID] = val // 更新旧值 缓存
-	}
-
-}
-
-func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	kv.mu.Lock()
-	fmt.Println("request " + strconv.FormatInt(args.RequestID, 10) + "get lock")
 	defer kv.mu.Unlock()
 
 	if args.Confirm {
-		// 是一个重复请求, 让你取删除缓存
-		if _, ok := kv.status[args.RequestID]; ok {
-			delete(kv.status, args.RequestID)
+		if _, ok := kv.status[args.Id]; ok {
+			delete(kv.status, args.Id)
 		}
-		return // 别忘记返回, 有的时候你就是忘记返回, 并且逻辑结果写的不严谨导致 出的bug
-
+		return
 	}
-	if _, ok := kv.status[args.RequestID]; ok { // 存在这个请求的旧值了
-		// 直接对这个请求返回上一次请求的结果即可, 因为归根到底是一个请求 , 必须保证只能执行一次!
-		reply.Value = kv.status[args.RequestID]
 
+	if _, ok := kv.status[args.Id]; ok {
+		reply.Value = kv.status[args.Id]
 	} else {
-		val := kv.data[args.Key]
-		kv.data[args.Key] = val + args.Value // 新的结果是 拼接的
+		val := kv.kvMap[key]
+		kv.kvMap[key] = args.Value
 		reply.Value = val
-		kv.status[args.RequestID] = val // 同时更新缓存结果
+		kv.status[args.Id] = val
 	}
 }
+
+func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
+	// Your code here.
+	key := args.Key
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if args.Confirm {
+		if _, ok := kv.status[args.Id]; ok {
+			delete(kv.status, args.Id)
+		}
+		return
+	}
+	if _, ok := kv.status[args.Id]; ok {
+		reply.Value = kv.status[args.Id]
+	} else {
+		val := kv.kvMap[key]
+		kv.kvMap[key] = val + args.Value
+		reply.Value = val
+		kv.status[args.Id] = val
+	}
+}
+
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
-	// 初始化服务器的数据存储（例如一个 map）
-	kv.data = make(map[string]string)
-
-	// 初始化锁，确保数据访问时的线程安全
-	kv.mu = sync.Mutex{}
-
-	// 如果需要支持重复请求检测，可以初始化一个请求ID的记录
+	// You may need initialization code here.
+	kv.kvMap = make(map[string]string)
 	kv.status = make(map[int64]string)
-	// 记录已处理的请求ID , 以及这个请求 上一次来的时候 的结果 是?
-
-	// 其他初始化代码，比如设置服务器的配置或日志
-	fmt.Println("KVServer started")
 
 	return kv
 }
